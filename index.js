4bbba43def8b59f27402884ded0d9c1d844cb272
archana <archanakrishna202@gmail.com>
2024-03-13 20:00:09 +0530

    m1

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+// Example JavaScript code
+document.addEventListener('DOMContentLoaded', function () {
+    // Your JavaScript code here
+    var greeting = "Hello, this is JavaScript!";
+    console.log(greeting);
+
+    // You can add more JavaScript code here
+});
+</script>
